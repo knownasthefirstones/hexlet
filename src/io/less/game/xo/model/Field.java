e5ac0ba719891b2fda755e8af9fe6426c278dbb9
thefirstones <knownasthefirstones@gmail.com>
2020-04-24 08:45:33 +0000

    Filed to be fieldSize flexible

--- src/io/less/game/xo/model/Field.java
+++ src/io/less/game/xo/model/Field.java
@@ -7,11 +7,19 @@ import java.awt.*;
 
 public class Field {
 
-    private static final int FIELD_SIZE = 3;    // задаем константу размера поля
-    private final Figure[][] field = new Figure[FIELD_SIZE][FIELD_SIZE];    // создаем поле с типом заполнености его значениями Figure
+//    private static final int FIELD_SIZE = 3;    // задаем константу размера поля
+//    private final Figure[][] field = new Figure[FIELD_SIZE][FIELD_SIZE];    // создаем поле с типом заполнености его значениями Figure
+
+    private final Figure[][] field;
+    private final int fieldSize;
+
+    public Field(final int fieldSize) {
+        this.fieldSize = fieldSize;
+        field = new Figure[fieldSize][fieldSize]; // поле заполняется массивом фигур
+    }
 
     public int getFieldSize() { // возвращаем размер поля
-        return FIELD_SIZE;
+        return fieldSize;
     }
 
     public Figure getFigure(final Point point) throws InvalidPointException {    // позвращаем фигуру с типом Figure, принимаем на вход метод Point
@@ -33,7 +41,7 @@ public class Field {
     }
 
     private boolean checkXYCoordinate(final int coordinate) {  // проверка X и Y по одинаковому правилу, поэтому сделаем отдельный метод и передадим выше
-        return coordinate >= 0 && coordinate <= FIELD_SIZE;     // ретернит true если координата валидна
+        return coordinate >= 0 && coordinate <= field.length;     // ретернит true если координата валидна
     }
 
 }
