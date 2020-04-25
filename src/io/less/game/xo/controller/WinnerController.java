d4530c31944dfcfdcf19a5e9755ff511494c2cb1
thefirstones <knownasthefirstones@gmail.com>
2020-04-25 22:37:32 +0300

    WinnerController adding check

--- src/io/less/game/xo/controller/WinnerController.java
+++ src/io/less/game/xo/controller/WinnerController.java
@@ -11,11 +11,11 @@ public class WinnerController {
     public Figure getWinner(final Field field) {
         // если фигура по полю 0,0 = фигуре по полю 0,1 и равна фигуре 0,2 то выводим фигуру по полю 0,0 как победитель
         try {
-            /*
-//            заменили на использование метода checkXYWinner();
+/*//            заменили на использование метода checkXYWinner();
             if (field.getFigure(new Point(0, 0)) == field.getFigure(new Point(0, 1)) &&
                 field.getFigure(new Point(0, 0)) == field.getFigure(new Point(0, 2)))
-                return field.getFigure(new Point(0, 0));
+                return field.getFigure(new Point(0, 0));*/
+
 //            добавим цикл на проверку point x y по горизонтали через увеличение значение i
             for (int i = 0; i < field.getFieldSize(); i++) {
                 if (checkXYWinner(field, new Point(i, 0), new Point(i, 1), new Point(i, 2))) {
@@ -34,7 +34,7 @@ public class WinnerController {
             }
             if (checkXYWinner(field, new Point(2, 0), new Point(2, 2), new Point(0, 2))) {
                 return field.getFigure(new Point(2, 0));
-            }*/
+            }
 
             /*for (int i = 0; i < field.getFieldSize(); i++) {
                 // рощенный способ реализации анонимного интерфейса
@@ -72,12 +72,13 @@ public class WinnerController {
                     return field.getFigure(new Point(0, i));
             }*/
 
-            for (int i = 0; i < field.getFieldSize(); i++) {
+            /*
+            for (int i = 0; i < 3; i++) {
                 if (checkXYWinner(field, new Point(i, 0), point -> new Point(point.x, point.y + 1)))
                     return field.getFigure(new Point(i, 0));
             }
-            for (int i = 0; i < field.getFieldSize(); i++) {
-                if (checkXYWinner(field, new Point(0, i), point -> new Point(point.x + 1, point.y)))
+            for (int i = 0; i < 3; i++) {
+                if (checkXYWinner(field, new Point(i, 0), point -> new Point(point.x + 1, point.y)))
                     return field.getFigure(new Point(0, i));
             }
 
@@ -85,6 +86,7 @@ public class WinnerController {
                 return field.getFigure(new Point(0, 0));
             if (checkXYWinner(field, new Point(0, 2), point -> new Point(point.x + 1, point.y - 1)))
                 return field.getFigure(new Point(1, 1));
+*/
 
         } catch (InvalidPointException e) {
             e.printStackTrace();
@@ -92,7 +94,7 @@ public class WinnerController {
         return null;
     }
 
-/*//     создаем метод, который проверяет значение поля1, поля2 и поля3
+//     создаем метод, который проверяет значение поля1, поля2 и поля3
     private boolean checkXYWinner(final Field field, final Point point1, final Point point2, final Point point3) {
         // заелючим в try catch так как иначе не даст вызвать getFigure
         try {
@@ -100,14 +102,16 @@ public class WinnerController {
                 return false;
             }
             if (field.getFigure(point1) == field.getFigure(point2) &&
-                    field.getFigure(point1) == field.getFigure(point2)) {
+                    field.getFigure(point1) == field.getFigure(point3)) {
                 return true;
+            } else {
+                return false;
             }
         } catch (InvalidPointException e) {
             e.printStackTrace();
         }
         return false;
-    }*/
+    }
 
 /*    // поменял метод на более удобный с использованием интерфейса по замене следующего поинта
     private boolean checkXYWinner(final Field field, final Point startPoint, final IPointChanger pointChanger) {
@@ -124,29 +128,32 @@ public class WinnerController {
         return false;
     }*/
 
-    // вызываем значение поля, сравниваем со следуюзим, далее меняем поит на следующий
+/*    // вызываем значение поля, сравниваем со следуюзим, далее меняем поит на следующий
     // вызываем значение по новому полю и сравниваем со следующим и так до конца поля
-    private boolean checkXYWinner(final Field field, final Point currentPoint, final IPointChanger pointChanger) {
+    private boolean checkXYWinner(final Field field,
+                                  final Point currentPoint,
+                                  final IPointChanger pointChanger) {
         final Figure currentFigure;
         final Figure nextFigure;
         final Point nextPoint = pointChanger.next(currentPoint);
         try {
             currentFigure = field.getFigure(currentPoint);
-            nextFigure = field.getFigure(currentPoint);
+            nextFigure = field.getFigure(nextPoint);
         } catch (final InvalidPointException e) { // ксли бросаем эксепшен то скорее всего мы дошли до конца поля
             return true;
         }
 
         if (currentFigure == null) return false;
+
         if (currentFigure != nextFigure) return false;
 
         return checkXYWinner(field, nextPoint, pointChanger);
-    }
+    } // чек проверяем пока мы не уперлись в конец поля
 
 
     // добавим Интерфейс поинт ченджер который будет на вход принимать поин и возвращать следующий поинт
     private interface IPointChanger {
         Point next(final Point point);
-    }
+    }*/
 
 }
